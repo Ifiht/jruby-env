c74df8b313c1cdd0082f8609330e40709fcdbf96
Peter Pan <peter@never.lan>
2021-12-05 03:54:47 +0000

    moved project, add standalone jruby

--- cis651finalapp/src/org/evlis/cis651finalapp/Main.java
+++
@@ -1,4 +0,0 @@
-package org.evlis.cis651finalapp;
-
-public class Main extends org.ruboto.EntryPointActivity {
-}
---
+++ cis651finalappv3/src/org/evlis/cis651finalappv3/MainActivity.java
@@ -0,0 +1,4 @@
+package org.evlis.cis651finalappv3;
+
+public class MainActivity extends org.ruboto.EntryPointActivity {
+}
